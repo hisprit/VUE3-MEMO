93ead27f07a1d71bbc64f50815bcb104c06f25cf
KimJongkyu <hisprit@naver.com>
2024-09-12 18:46:42 +0900

    first commit

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
 
-const memos = ["내용1", "내용2", "내용3"]
-app.get('/memos', (req, res) => {
+app.use(bodyParser.json())
+
+const memos = []
+app.get('/api/memos', (req, res) => {
   res.send(memos)
 })
+app.post('/api/memos', (req, res) =>{
+  memos.push(req.body.content);
+  res.send(memos)
+}) 
+app.put('/api/memos/:idx', (req, res) =>{
+  memos[req.params.idx] = req.body.content;
+  res.send(memos)
+}) 
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
